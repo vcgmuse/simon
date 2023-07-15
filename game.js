250644ceae3fcd626d8203d7358db4bfcebd7088
vcgmuse <vcgmuse@gmail.com>
2023-07-15 17:59:27 -0500

    TEST

--- game.js
+++ game.js
@@ -6,13 +6,15 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
-document.addEventListener("touchstart", function(event) {
+
+$(document).addEventListener("touchstart", function(event){
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
-});
+
+})
 $(document).keypress(function(event) {
   if (!started) {
     $("#level-title").text("Level " + level);
