f9a6b6a8ccf9da831e8983af0abb4a781518033a
vcgmuse <vcgmuse@gmail.com>
2023-07-17 08:31:19 -0500

    Should all be working needs testing for linux systems.

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 var buttonColours = ["red", "blue", "green", "yellow", "purple", "orange"];
 
 var gamePattern = [];
@@ -6,8 +5,10 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
+var score = 0;
+var topTen = [];
 
-$(document).keypress(function() {
+$(document).keypress(function () {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
@@ -15,38 +16,48 @@ $(document).keypress(function() {
   }
 });
 
-$(".btn").click(function() {
-
+$(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
 
-  checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function checkAnswer(currentLevel) {
-
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-    } else {
-      playSound("wrong");
-      $("body").addClass("game-over");
-      $("#level-title").text("Game Over, A to Restart");
-
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    score++;
+    $("#score").text(score);
+    if (userClickedPattern.length === gamePattern.length) {
       setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-
-      startOver();
+        nextSequence();
+      }, 1000);
     }
-}
+  } else {
+    playSound("wrong");
+    var name = prompt("Enter your name.");
+    topTen.push({ name: name, score: score });
+    $(".score-board").html("<h3>Top 10 Scores</h3>");
+    topTen.sort(function (a, b) {
+      return b.score - a.score;
+    });
 
+    topTen.forEach((top) => {
+      $(".score-board").append(`<div class="top-scores">${top.name +"  " + top.score}</div>`);
+    });
+    score = 0;
+    $("#score").text("0");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, A to Restart");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    startOver();
+  }
+}
 
 function nextSequence() {
   userClickedPattern = [];
@@ -57,9 +68,12 @@ function nextSequence() {
   gamePattern.push(randomChosenColour);
 
   var i = 0;
-  var intervalId = setInterval(function() {
+  var intervalId = setInterval(function () {
     var colorToPlay = gamePattern[i];
-    $("#" + colorToPlay).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + colorToPlay)
+      .fadeIn(100)
+      .fadeOut(100)
+      .fadeIn(100);
     playSound(colorToPlay);
     i++;
     if (i >= gamePattern.length) {
@@ -68,10 +82,9 @@ function nextSequence() {
   }, 700);
 }
 
-
 function playGamePattern(pattern) {
   var i = 0;
-  var intervalId = setInterval(function() {
+  var intervalId = setInterval(function () {
     animatePress(pattern[i]);
     playSound(pattern[i]);
     i++;
@@ -80,7 +93,6 @@ function playGamePattern(pattern) {
     }
   }, 500); // Adjust the delay time between each color as needed
 }
-
 
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
